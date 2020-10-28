23ea7783397e8e270b835ab29a6f9c005f00e2a4
Brandie Burditt <brandieburditt@gmail.com>
2020-10-28 17:25:16 -0400

    completed min-jumps

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,36 @@
 'use strict';
-
-// Complete this algo
-const minJumps = arr => {
-
+const findBiggestNum = (array) => {
+  let biggestNum = 0;
+  let index = 0;
+  for (let i = 0; i < array.length; i++) {
+    let currentElement = array[i];
+    if (currentElement >= biggestNum) {
+      biggestNum = currentElement;
+      index = i;
+    }
+  }
+  return index + 1;
 };
 
-module.exports = minJumps
+// Complete this algo
+const minJumps = (arr) => {
+  let counter = 0;
+  let i = 0;
+
+  while (i < arr.length) {
+    let currentNum = arr[i];
+    if (currentNum >= arr.length - 1 - i) {
+      counter++;
+      return counter;
+    } else {
+      let choices = arr.slice(i + 1, i + currentNum + 1);
+      counter++;
+      let newIndex = findBiggestNum(choices);
+      i += newIndex;
+    }
+  }
+
+  return counter;
+};
+
+module.exports = minJumps;
